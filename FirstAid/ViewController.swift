58e07c0c5fe33303eeb4483ed13954bfc5b1f54c
Richer Larivière <richerlariviere@gmail.com>
2017-07-14 21:03:33 -0400

    Initial Commit

---
+++ FirstAid/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstAid
+//
+//  Created by Richer Larivière on 17-07-14.
+//  Copyright © 2017 Help. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
